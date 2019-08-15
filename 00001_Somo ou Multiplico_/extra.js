31d7f65356ff77c70f012ab8ade16b46d035abe6
ssiqueira@digitalhouse.com <ssiqueira@digitalhouse.com>
2019-08-15 16:37:41 +0000

    Mumuki Export on 2019-08-15 16:37:41 +0000

---
+++ 00001_Somo ou Multiplico_/extra.js
@@ -0,0 +1,17 @@
+function somatoria(array) {
+	var resul = 0
+	for (var i = 0; i < array.length; i++) {
+	  resul += array[i]
+	}
+	
+	return resul
+  }
+  
+  function produto(array) {
+	var resul = 1
+	for (var i = 0; i < array.length; i++) {
+	  resul *= array[i]
+	}
+	
+	return resul
+  }
