05f89f376b3eefb1e6463426f01dd9b9681a3baa
ssiqueira@digitalhouse.com <ssiqueira@digitalhouse.com>
2019-08-16 18:02:09 +0000

    Mumuki Export on 2019-08-16 18:02:09 +0000

---
+++ 00005_Novo exeradasasdcício/extra.js
@@ -0,0 +1,17 @@
+var musicas = [
+	{
+	  id: 1,
+	  nome: "Radio Ga Ga",
+	  pontuacao: 8.5
+	},
+	{
+	  id: 2,
+	  nome: "Birds",
+	  pontuacao: 8.1
+	},
+	{
+	  id: 3,
+	  nome: "Believer",
+	  pontuacao: 7.8
+	}
+]
